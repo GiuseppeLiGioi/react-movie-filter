df93b551bcf960b07d4d29fe2db2efc2d7e0fd68
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-02-25 15:15:20 +0100

    add: main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Main from "../src/assets/components/Main"
 
 
 function App() {
@@ -6,7 +7,10 @@ function App() {
 
   return (
     <>
-   
+    
+
+
+     <Main />  
     </>
   )
 }
